df40a3ff89fb76e4874dec72c675bb3605d728fa
Yuvraj Singh <yuvraj0157@gmail.com>
2022-11-08 19:20:31 +0530

    Added API view organization by ID

--- api/routes/OrganizationRoutes.js
+++ api/routes/OrganizationRoutes.js
@@ -2,7 +2,7 @@ const express=require("express");
 const Router=express.Router();
 const mysqlConnection=require("../utils/connection");
 
-Router.get("/",(req,res)=>{
+Router.get("/all",(req,res)=>{
     mysqlConnection.query("SELECT * FROM organization",(err,rows,fields)=>{
         if(!err)
         {
@@ -15,4 +15,19 @@ Router.get("/",(req,res)=>{
     })
 })
 
+Router.get("/view/:id",(req,res)=>{
+
+    const orgid=req.params.id;
+    
+    mysqlConnection.query("SELECT organization.username,organization.name,organization.domain FROM organization WHERE organization.id=?",[orgid],(err,rows,fields)=>{
+        if(!err)
+        {
+            res.send(rows);
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
+})
 module.exports=Router;
