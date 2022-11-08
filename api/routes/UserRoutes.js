57b0583083fde1b3fc8021c27b958966196d88e3
Shubh-Bindal <20bcs121@iiitdwd.ac.in>
2022-11-08 14:22:55 +0530

    /api/user/view/:id Route Added

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -32,4 +32,20 @@ Router.get("/all/:orgid",(req,res)=>{
         }
     })
 })
+
+Router.get("/all/view/:id",(req,res)=>{
+
+    const userid=req.params.id;
+
+    mysqlConnection.query("SELECT user.id, user.name,user.organization,user.verified,user.imageurl FROM user WHERE user.id='"+userid+"'",(err,rows,fields)=>{
+        if(!err)
+        {
+            res.send(rows);
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
+})
 module.exports=Router;
