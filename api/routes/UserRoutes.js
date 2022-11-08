5cb4c163ae6417e1d7ab7f8e014bc87626fc6513
Shubh-Bindal <20bcs121@iiitdwd.ac.in>
2022-11-08 13:14:20 +0530

    /api/user/all/:orgid Route Added

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const Router=express.Router();
 const mysqlConnection=require("../utils/connection");
 
+
+
 Router.get("/all",(req,res)=>{
     mysqlConnection.query("SELECT id, name, email, organization,verified,imageurl FROM user",(err,rows,fields)=>{
         if(!err)
@@ -15,4 +17,19 @@ Router.get("/all",(req,res)=>{
     })
 })
 
+Router.get("/all/:orgid",(req,res)=>{
+
+    const orgid=req.params.orgid;
+
+    mysqlConnection.query("SELECT user.id, user.name,user.organization,user.verified,user.imageurl FROM user INNER JOIN organization ON user.organization=organization.id WHERE organization.id='"+orgid+"'",(err,rows,fields)=>{
+        if(!err)
+        {
+            res.send(rows);
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
+})
 module.exports=Router;
