12ba9d7789a65d9f950fc83ad7033bfa6e874e18
Yuvraj Singh <yuvraj0157@gmail.com>
2022-11-09 18:49:02 +0530

    Added endpoint create an organization

--- api/routes/OrganizationRoutes.js
+++ api/routes/OrganizationRoutes.js
@@ -2,6 +2,75 @@ const express=require("express");
 const Router=express.Router();
 const mysqlConnection=require("../utils/connection");
 
+Router.post('/create', function (req, res) {
+    var postData = req.body;
+    const {username,name,domain} = postData;
+    console.log(username);
+    console.log(name);
+    console.log(domain);
+    /*
+    // validate request
+    if(!req.body){
+        res.status(404);
+        res.json(
+            {
+                code: 1700,
+                msg: "Empty Field"
+            }
+        );
+        return;
+    }
+    */
+    mysqlConnection.query("INSERT INTO organization (username, name, domain) VALUES (?, ?, ?)", [username,name,domain],(err, rows, fields)=>{
+        if(!err)
+        {
+            res.json(
+                {
+                    code: 1200,
+                    msg: "Organization Created"
+                }    
+            );
+        }
+        else
+        {
+            console.log(err);
+            const myerr = err.errno;
+            res.status(404);
+
+            if(myerr==1062)
+            {
+                res.json(
+                    {
+                        code: 1500,
+                        msg: "Organization Already Exists"
+                    }
+                );
+            }
+            else if(myerr==1048)
+            {
+                res.status(404);
+                res.json(
+                    {
+                        code: 1700,
+                        msg: "Empty Field"
+                    }
+                );
+                return;  
+            }
+            else
+            {
+                res.json(
+                    {
+                        code: 1600,
+                        msg: "Internal Error"
+                    }
+                );
+            }
+            
+        }
+    })
+})
+
 Router.get("/all",(req,res)=>{
     mysqlConnection.query("SELECT * FROM organization",(err,rows,fields)=>{
         if(!err)
