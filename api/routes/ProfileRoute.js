e6f53ff0441d46a533b32b9c4f6797315ae970d3
Yuvraj Singh <yuvraj0157@gmail.com>
2022-11-12 19:31:06 +0530

    Added endpoint view profile of a person

--- api/routes/ProfileRoute.js
+++ api/routes/ProfileRoute.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const Router = express.Router();
 const mysqlConnection = require("../utils/connection");
@@ -31,5 +32,126 @@ Router.get("/friends/:userid",async (req,res)=>{
         }
     })
 })
+
+function isPersonMyFriend(first, second)
+{
+    return new Promise(resolve=>{
+        mysqlConnection.query("SELECT friends.id From friends WHERE friends.first=? AND friends.second=?",[first,second],(err,rows,fields)=>{
+
+            if(!err && rows.length!=0)
+            {
+                resolve(true);
+            }
+            else
+            {
+                resolve(false);
+            }
+        })
+    });
+}
+function incomingRequest(first, second)
+{
+    return new Promise(resolve=>{
+        mysqlConnection.query("SELECT request_incoming.id From request_incoming WHERE request_incoming.first=? AND request_incoming.second=?",[first,second],(err,rows,fields)=>{
+            if(!err && rows.length!=0)
+            {
+                resolve(true);
+            }
+            else
+            {
+                resolve(false);
+            }
+        })
+    });
+}
+function outgoingRequest(first, second)
+{
+    return new Promise(resolve=>{
+        mysqlConnection.query("SELECT request_outgoing.id From request_outgoing WHERE request_outgoing.first=? AND request_outgoing.second=?",[first,second],(err,rows,fields)=>{
+            if(!err && rows.length!=0)
+            {
+                resolve(true);
+            }
+            else
+            {
+                resolve(false);
+            }
+        })
+
+    });
+}
+
+Router.get("/view/:personid/:userid", async(req, res) => {
+
+    const userid = req.params.userid;
+    const personid = req.params.personid;
+    const query_string = `SELECT user.id as person_id,user.name as person_name,user.imageurl,user.email as person_email
+    FROM user
+    WHERE user.id = ?`
+
+    mysqlConnection.query(query_string,[personid],async (err, rows, fields) => {
+        if (!err) {
+            const isFriend = await isPersonMyFriend(userid,personid);
+            if(rows.length != 0 && isFriend)
+            {
+                obj = {
+                    "friend": true,
+                    "request": false,
+                    "request-type": null,
+                }
+                let result = Object.values(JSON.parse(JSON.stringify(rows)));
+                // console.log(Object.assign(...result,obj));
+                let z = Object.assign(...result,obj);
+                res.send(z);
+            }
+            else if(rows.length != 0 && !isFriend)
+            {
+                const incoming = await incomingRequest(userid,personid);
+                const outgoing = await outgoingRequest(userid,personid);
+                if(incoming)
+                {
+                    obj = {
+                        "friend": false,
+                        "request": true,
+                        "request-type": `incoming`
+                    }
+                    let result = Object.values(JSON.parse(JSON.stringify(rows)));
+                    let z = Object.assign(...result,obj);
+                    res.send(z);
+                }
+                else if(outgoing)
+                {
+                    obj = {
+                        "friend": false,
+                        "request": true,
+                        "request-type": `outgoing`
+                    }    
+                    let result = Object.values(JSON.parse(JSON.stringify(rows)));
+                    let z = Object.assign(...result,obj);
+                    res.send(z);
+                }
+                else
+                {
+                    obj = {
+                        "friend": false,
+                        "request": false,
+                        "request-type": null
+                    }    
+                    let result = Object.values(JSON.parse(JSON.stringify(rows)));
+                    let z = Object.assign(...result,obj);
+                    res.send(z);
+                }
+            }
+            else{
+                res.sendStatus(404);
+            }
+        }
+        else {
+            res.sendStatus(404);
+        }
+    })
+})
+
+
 
 module.exports = Router;
