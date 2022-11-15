2708315434e3244d98574eb9a2ed0431dddae4cd
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2022-11-15 19:53:47 +0530

    Update connection.js

--- api/utils/connection.js
+++ api/utils/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mysql = require("mysql")
-var mysqlConnection = mysql.createConnection({
+var mysqlConnection = mysql.createPool({
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
@@ -9,15 +9,15 @@ var mysqlConnection = mysql.createConnection({
 });
 
 
-mysqlConnection.connect((err)=>{
-    if(!err)
-    {
-        console.log("Database Connected!");
-    }
-    else
-    {
-        console.log("Database Connection Failed!");
-    }
-})
+// mysqlConnection.connect((err)=>{
+//     if(!err)
+//     {
+//         console.log("Database Connected!");
+//     }
+//     else
+//     {
+//         console.log("Database Connection Failed!");
+//     }
+// })
 
 module.exports=mysqlConnection;
