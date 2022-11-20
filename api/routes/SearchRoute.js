f1190cdba3ee1c645ab714c2f248ec3455c37fec
Yuvraj Singh <yuvraj0157@gmail.com>
2022-11-20 11:19:26 +0530

    Added user's email in search endpoint

--- api/routes/SearchRoute.js
+++ api/routes/SearchRoute.js
@@ -12,7 +12,7 @@ Router.get("/:name",requireAuth, (req, res) => {
     console.log(myname);
     console.log(userid);
     console.log(searchedExpression);
-    const query_string = `SELECT user.id,user.name,user.imageurl
+    const query_string = `SELECT user.id,user.name,user.imageurl,user.email
     FROM user
     where user.organization = (select user.organization from user where user.id = ?) and user.name LIKE ?  && user.id != ? and user.verified != 0`
     mysqlConnection.query(query_string,[userid,searchedExpression,userid],(err, rows, fields) => {
